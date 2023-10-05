b8b049507a99563ab2290d06ec702074ca1e7492
Joko Endriyanto <46419237+jokoendriyanto@users.noreply.github.com>
2023-10-05 10:40:18 +0700

    first file comit

--- 1. Dart Pertamaku.dart
+++ 1. Dart Pertamaku.dart
@@ -1,6 +1,17 @@
 void main() {
-  String nama = "Joko Endriyanto";
-  var motivasi = "Belajar Dart Dengan Gembira";
+  // Ini adalah kode dart pertamaku
 
+  String nama = "Joko Endriyanto"; //membuat variabel string nama
+  //Statically typed, di mana kita perlu mendefinisikan variabel sebelum bisa menggunakannya.
+
+  var motivasi = "Belajar Dart Dengan Gembira";
+  //Type inference, di mana tipe data tidak perlu didefinisikan secara eksplisit.
+
+  var angka1 = 10;
+  var angka2 = 200;
+
+// Menampilkan hasil di console
   print("Haloo Saya " "$nama " "$motivasi");
+
+  print(angka1 * angka2);
 }
